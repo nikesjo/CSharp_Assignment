dc9ab28b9a6a8bb1b550475fefd0b9c9e21cde31
Nike <nikesjolin@outlook.com>
2023-12-07 18:10:09 +0100

    Created IFileService and made summary comments

--- ClassLibrary.Shared/Interfaces/IFileService.cs
+++ ClassLibrary.Shared/Interfaces/IFileService.cs
@@ -2,4 +2,18 @@
 
 public interface IFileService
 {
+    /// <summary>
+    /// Save contact to a filepath.
+    /// </summary>
+    /// <param name="filepath">Enter the filepath with extension (eg. d:\Education\csharp\assignment\myfile.json)</param>
+    /// <param name="contact">Enter the contact as a string.</param>
+    /// <returns>Returns true if saved, or false if failed.</returns>
+    bool SaveContactToFile(string filepath, string contact);
+
+    /// <summary>
+    /// Get content as string from a filepath.
+    /// </summary>
+    /// <param name="filepath">Enter the filepath with extension (eg. d:\Education\csharp\assignment\myfile.json)</param>
+    /// <returns>Returns file content as string if file exists, or returns null.</returns>
+    string GetContentFromFile(string filepath);
 }

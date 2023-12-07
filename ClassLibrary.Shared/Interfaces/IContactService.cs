564b193ee8a3f1e1795f7d481393877529a7bd7d
Nike <nikesjolin@outlook.com>
2023-12-07 17:44:41 +0100

    Added summary comments to IContactService

--- ClassLibrary.Shared/Interfaces/IContactService.cs
+++ ClassLibrary.Shared/Interfaces/IContactService.cs
@@ -2,7 +2,23 @@
 
 internal interface IContactService
 {
+    /// <summary>
+    /// Add a contact to the contact list.
+    /// </summary>
+    /// <param name="contact">A contact of type IContact</param>
+    /// <returns>Return true if successful, or false if it fails or customer already exists.</returns>
     bool AddContactToList(IContact contact);
+
+    /// <summary>
+    /// Gets all contacts from contact list.
+    /// </summary>
+    /// <returns>Return contacts if list has contacts in it, if no contacts exists return null.</returns>
     IEnumerable<IContact> GetContactsFromList();
+
+    /// <summary>
+    /// Get a specific contact from contact list by email.
+    /// </summary>
+    /// <param name="email">Enter the email as a string.</param>
+    /// <returns>Return contact if exists, if contact does not exists return null.</returns>
     IContact GetContactFromList(string email);
 }

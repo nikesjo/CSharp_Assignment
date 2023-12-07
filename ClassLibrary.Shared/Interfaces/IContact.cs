d3af8560415a23196215693336d1b09ecafa3fdd
Nike <nikesjolin@outlook.com>
2023-12-07 17:24:56 +0100

    Created IContact and IContactService

---
+++ ClassLibrary.Shared/Interfaces/IContact.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary.Shared.Interfaces;
+
+internal interface IContact
+{
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string PhoneNumber { get; set; }
+    string Email { get; set; }
+    string Address { get; set; }
+    string PostalCode { get; set; }
+    string City { get; set; }
+}
---
+++ ClassLibrary.Shared/Interfaces/IContactService.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassLibrary.Shared.Interfaces;
+
+internal interface IContactService
+{
+    bool AddContactToList(IContact contact);
+    IEnumerable<IContact> GetContactsFromList();
+    IContact GetContactFromList(string email);
+}

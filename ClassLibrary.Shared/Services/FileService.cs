59efbafc80906519a2dd519f3069810bc594376a
Nike <nikesjolin@outlook.com>
2023-12-08 16:22:26 +0100

    Created FileService

---
+++ ClassLibrary.Shared/Services/FileService.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary.Shared.Interfaces;
+using System.Diagnostics;
+
+namespace ClassLibrary.Shared.Services;
+
+public class FileService : IFileService
+{
+    public string GetContentFromFile(string filepath)
+    {
+        try
+        {
+            if (File.Exists(filepath))
+            {
+                return File.ReadAllText(filepath);
+            }
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return null!;
+    }
+
+    public bool SaveContactToFile(string filepath, string contact)
+    {
+        try
+        {
+            using var sw = new StreamWriter(filepath);
+            sw.WriteLine(contact);
+            return true;
+        }
+        catch (Exception ex) { Debug.WriteLine(ex.Message); }
+        return false;
+    }
+}

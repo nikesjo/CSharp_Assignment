789eedbbdc041ba2295a6cc3926073abc4229d08
Nike <nikesjolin@outlook.com>
2023-12-08 16:00:30 +0100

    Created ContactService

---
+++ ClassLibrary.Shared/Services/ContactService.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary.Shared.Interfaces;
+using ClassLibrary.Shared.Models;
+
+namespace ClassLibrary.Shared.Services;
+
+public class ContactService : IContactService
+{
+    private List<Contact> _contactlist = [];
+
+    public bool AddContactToList(IContact contact)
+    {
+        return true;
+    }
+
+    public IContact GetContactFromList(string email)
+    {
+        return null!;
+    }
+
+    public IEnumerable<IContact> GetContactsFromList()
+    {
+        return null!;
+    }
+}

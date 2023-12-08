aab6f265f9f3b50cfb3982677747b5ca04b10046
Nike <nikesjolin@outlook.com>
2023-12-08 17:35:23 +0100

    Created MenuService with ShowMainMenu

---
+++ ConsoleApp_AdressBook/Services/MenuService.cs
@@ -0,0 +1,44 @@
+﻿using ClassLibrary.Shared.Interfaces;
+using ClassLibrary.Shared.Services;
+
+namespace ConsoleApp_AddressBook.Services;
+
+public class MenuService
+{
+    private readonly IContactService _contactService = new ContactService();
+
+    public void ShowMainMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("Menu\n");
+            Console.WriteLine("1. Add a contact");
+            Console.WriteLine("2. Show all contacts");
+            Console.WriteLine("3. Search a specific contact");
+            Console.WriteLine("4. Exit");
+
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    ShowMainMenu();
+                    break;
+                case "2":
+                    ShowMainMenu();
+                    break;
+                case "3":
+                    ShowMainMenu();
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, please try again");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

decc5b095e53e5b578b6c241b5c7bd11b4e05169
Nike <nikesjolin@outlook.com>
2023-12-14 13:32:39 +0100

    Added a new maui project and linked to class library

---
+++ Addressbook/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Addressbook
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

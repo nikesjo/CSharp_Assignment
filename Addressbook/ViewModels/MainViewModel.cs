d9504a1f8e63f6dca84e4078ea2735719f52b659
Nike <nikesjolin@outlook.com>
2023-12-14 15:21:37 +0100

    Added RelayCommand navigation in MVM

--- Addressbook/ViewModels/MainViewModel.cs
+++ Addressbook/ViewModels/MainViewModel.cs
@@ -1,8 +1,37 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Addressbook.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
+    [RelayCommand]
+    private async Task NavigateToAddContact()
+    {
+        await Shell.Current.GoToAsync("");
+    }
 
+    [RelayCommand]
+    private async Task NavigateToList()
+    {
+        await Shell.Current.GoToAsync("");
+    }
+
+    [RelayCommand]
+    private async Task NavigateToContactDetails()
+    {
+        await Shell.Current.GoToAsync("");
+    }
+
+    [RelayCommand]
+    private async Task NavigateToRemove()
+    {
+        await Shell.Current.GoToAsync("");
+    }
+
+    [RelayCommand]
+    private async Task NavigateToUpdate()
+    {
+        await Shell.Current.GoToAsync("");
+    }
 }

615a8947c4f558eda4fffcc373744329ceac04f4
Nike <nikesjolin@outlook.com>
2023-12-14 13:07:01 +0100

    Added maui project

---
+++ Maui_AddressBook/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_AddressBook
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui_AddressBook/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_AddressBook
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

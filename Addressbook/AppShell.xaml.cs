8ee18334d1af2e2c155d7e402df7ebf8b25b2da5
Nike <nikesjolin@outlook.com>
2023-12-14 15:47:48 +0100

    Registered pages in AppShell

--- Addressbook/AppShell.xaml.cs
+++ Addressbook/AppShell.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace Addressbook
+﻿using Addressbook.Views;
+
+namespace Addressbook
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(AddContactPage), typeof(AddContactPage));
+            Routing.RegisterRoute(nameof(ContactListPage), typeof(ContactListPage));
+            Routing.RegisterRoute(nameof(ContactDetailsPage), typeof(ContactDetailsPage));
+            Routing.RegisterRoute(nameof(RemoveContactPage), typeof(RemoveContactPage));
+            Routing.RegisterRoute(nameof(UpdateContactPage), typeof(UpdateContactPage));
         }
     }
 }

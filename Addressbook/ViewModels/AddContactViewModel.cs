130fc656ca5e3b33440f996b76f2e7427f2d9821
Nike <nikesjolin@outlook.com>
2023-12-20 11:52:05 +0100

    Fixed uses of IContact interface for AddContactViewModel

--- Addressbook/ViewModels/AddContactViewModel.cs
+++ Addressbook/ViewModels/AddContactViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using ClassLibrary.Shared.Interfaces;
-using ClassLibrary.Shared.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Contact = ClassLibrary.Shared.Models.Contact;
@@ -8,23 +7,21 @@ namespace Addressbook.ViewModels;
 
 public partial class AddContactViewModel : ObservableObject
 {
-    //Borde detta vara IContactService?
-    private readonly ContactService _contactService;
+    private readonly IContactService _contactService;
 
-    //Borde detta vara IContactService?
-    public AddContactViewModel(ContactService contactService)
+    public AddContactViewModel(IContactService contactService)
     {
         _contactService = contactService;
     }
 
     [ObservableProperty]
-    private IContact _addContactForm = new();
+    private IContact? _addContactForm;
 
     [RelayCommand]
     private async Task AddContactToList()
     {
-        _contactService.AddContactToList(AddContactForm);
-        AddContactForm = new();
+        _contactService.AddContactToList(AddContactForm!);
+        AddContactForm = new Contact();
 
         await Shell.Current.GoToAsync("..");
     }

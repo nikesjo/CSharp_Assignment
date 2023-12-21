f96d5070d5be6bff65eabe6dc503d93d28a5b5af
Nike <nikesjolin@outlook.com>
2023-12-21 18:03:53 +0100

    Added code in ContactListViewModel

--- Addressbook/ViewModels/ContactListViewModel.cs
+++ Addressbook/ViewModels/ContactListViewModel.cs
@@ -1,5 +1,42 @@
-﻿namespace Addressbook.ViewModels;
+﻿using ClassLibrary.Shared.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
-class ContactListViewModel
+namespace Addressbook.ViewModels;
+
+public partial class ContactListViewModel : ObservableObject
 {
+    private readonly IContactService _contactService;
+
+    public ContactListViewModel(IContactService contactService)
+    {
+        _contactService = contactService;
+        UpdateContactList();
+
+        _contactService.ContactsUpdated += (sender, e) =>
+        {
+            UpdateContactList();
+        };
+    }
+
+    private void UpdateContactList()
+    {
+        ContactList = new ObservableCollection<IContact>(_contactService.GetContactsFromList());
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<IContact> contactList = [];
+
+    [RelayCommand]
+    private async Task NavigateToAddContact(IContact contact)
+    {
+        await Shell.Current.GoToAsync("AddContactView");
+    }
+
+    [RelayCommand]
+    private async Task NavigateToContactDetails(IContact contact)
+    {
+        await Shell.Current.GoToAsync("ContactDetailsView");
+    }
 }

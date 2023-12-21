01bd77ce2aef2bd0c483c998059c1ca05e04644d
Nike <nikesjolin@outlook.com>
2023-12-21 15:34:17 +0100

    n

--- Addressbook/ViewModels/ContactDetailsViewModel.cs
+++ Addressbook/ViewModels/ContactDetailsViewModel.cs
@@ -1,13 +1,36 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using ClassLibrary.Shared.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace Addressbook.ViewModels;
 
 public partial class ContactDetailsViewModel : ObservableObject
 {
-    [RelayCommand]
-    private async Task NavigateToUpdate()
+    private readonly IContactService _contactService;
+
+    public ContactDetailsViewModel(IContactService contactService)
     {
-        await Shell.Current.GoToAsync("");
+        _contactService = contactService;
+    }
+
+    [ObservableProperty]
+    private IContact? _contactDetailsView;
+
+    [RelayCommand]
+    private async Task NavigateToUpdate(IContact contact)
+    {
+        var parameters = new ShellNavigationQueryParameters
+        {
+            
+        };
+        await Shell.Current.GoToAsync("UpdateContactPage");
+    }
+    //RemoveCommand, navigate to UpdateContact, get and show a specific contact
+
+    [RelayCommand]
+    private void RemoveContactFromList(IContact contact)
+    {
+        _contactService.RemoveContactFromList(contact);
+
     }
 }
